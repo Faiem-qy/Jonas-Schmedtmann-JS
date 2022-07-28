37eec1e2119c21de5e6e12db5a472c2ebcbb47ff
Faiem-qy <110076359+Faiem-qy@users.noreply.github.com>
2022-07-28 14:27:35 -0400

    Update assignments.js

--- Fundamentals-Part1-1.11/assignments.js
+++ Fundamentals-Part1-1.11/assignments.js
@@ -2,19 +2,15 @@
 const country = "Trinidad";
 const continent = "South America";
 let population = 3;
-
 const isIsland = true;
 let language = "English";
-
 console.log(country);
 console.log(continent);
 console.log(population);
-
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
-
 console.log(population / 2);
 population++;
 console.log(population);
@@ -31,3 +27,12 @@ const description =
 console.log(description);
 */
 
+//BMI = mass / height ** 2 = mass / (height * height)//(mass in kg and height in meter)
+let markMass = 78;
+let markHeight = 1.69;
+let johnMass = 92;
+let jonsHeight = 1.95;
+
+BMI = markMass / markHeight ** 2 = markMass / (markHeight * markHeight);
+
+BMI = mass / height ** 2 = mass / (height * height)
