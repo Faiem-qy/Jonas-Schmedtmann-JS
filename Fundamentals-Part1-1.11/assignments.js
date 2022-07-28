c811e8469c083831d2d412aa7c7e00b7ef6e6569
Faiem-qy <110076359+Faiem-qy@users.noreply.github.com>
2022-07-27 23:32:45 -0400

    Add files via upload

---
+++ Fundamentals-Part1-1.11/assignments.js
@@ -0,0 +1,33 @@
+/*
+const country = "Trinidad";
+const continent = "South America";
+let population = 3;
+
+const isIsland = true;
+let language = "English";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population < 6);
+console.log(population < 33);
+const description =
+    country +
+    ' is in ' +
+    continent +
+    ' and it\'s ' +
+    population +
+    ' million people speak ' +
+    language;
+console.log(description);
+*/
+
---
+++ Fundamentals-Part1-1.11/script.js
@@ -0,0 +1,47 @@
+
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda"
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let jonas_matilda = "JM"
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+*/
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun)
+
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+
+year = 1991;
+console.log(typeof year)
+
+console.log(typeof null);
