ca6ea9abcf82f349b01a72b0ef1c42b9a7690de3
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-07-31 16:45:23 -0400

    Update script.js

--- Fundamentals-Part1-1.11/script.js
+++ Fundamentals-Part1-1.11/script.js
@@ -116,8 +116,7 @@ console.log(ageJonas, ageSarah, averageAge);
 
 // 1.16
 
-
-// /*
+/*
 let markMass = 95;
 let markHeight = 1.88;
 let johnMass = 85;
@@ -240,4 +239,89 @@ if (markBMI > johnBMI) {
 } else {
     console.log(`John's (${johnBMI}) BMI is higher than Mark's!(${markBMI})`)
 }
+*/
+
+/*
+// 1.20 Type Conversion and Coercion
+
+//  tyep conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18)
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old')
+console.log('I am ' + '23' + ' years old')
+console.log('I am ' + String(23) + ' years old')
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+
+let n = '1' + 1; // '11'
+n = n - 1;// 
+console.log(n);
+
+*/
+/*
+// 1.21 Truthy and falsy values
+// 5 falsy values; 0, '', underined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log("Dont't spend it all;");
+} else {
+    console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED')
+}
+
+*/
+
+/*
+// 1.22 Equality operators
+
+const age = '18';
+if (age === 18) console.log('You just became an adult :D(strict)');
+
+if (age == 18) console.log('You just became an adult :D(loose)');
+
+
+// operator for equality
+
+const favourite = Number(prompt(" What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {//22 === 23 -> flase
+    console.log('Cool! 23 is an amazing number!');
+} else if (favourite === 7) {
+    console.log('7 is also a cool number')
+} else if (favourite === 9) {
+    console.log('9 is also a cool number')
+}
+else {
+    console.log('Number is not 23 or 7 or 9')
+}
+
+// operator for different
+
+if (favourite !== 23)
+console.log('Why not 23?');
+
 */
