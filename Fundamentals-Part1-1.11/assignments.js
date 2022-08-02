bd32d84032a15bfc2eeb7b6c272dec67313a789b
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-08-02 11:04:40 -0400

    Update assignments.js

--- Fundamentals-Part1-1.11/assignments.js
+++ Fundamentals-Part1-1.11/assignments.js
@@ -27,38 +27,32 @@ const description =
 console.log(description);
 */
 
-// 1.16
+
 //BMI = mass / height ** 2 = mass / (height * height)//(mass in kg and height in meter)
 
 /*
+
 let markMass = 78;
 let markHeight = 1.69;
 let johnMass = 92;
 let johnHeight = 1.95;
-*/
 
 
-
-
-
-let markMass = 95;
-let markHeight = 1.88;
-let johnMass = 85;
-let johnHeight = 1.76;
+// let markMass = 95;
+// let markHeight = 1.88;
+// let johnMass = 85;
+// let johnHeight = 1.76;
 
 let markBMI = markMass / markHeight ** 2;
 let johnBMI = johnMass / johnHeight ** 2;
 
 let markHigherBMI = markBMI > johnBMI;
 
-console.log(markBMI, johnBMI)
-
 console.log(markBMI)
 console.log(johnBMI)
 console.log(markHigherBMI)
 // or console.log(markBMI, johnBMI, markHigherBMI);
 
-/*
 let data1 =
     "Mark's weights " +
     markMass +
@@ -70,7 +64,23 @@ let data1 =
     johnHeight +
     ' m tall'
 console.log(data1);
+
+const data2 = `Mark's BMI (${markBMI}) is higher than John's (${johnBMI})`;
+
+// another way is
+// console.log (`Mark's weight $(markMass) kg and is $(markHeight) m tall. Hohn's weight $(johnMass) kg and is $(johnHeight) m tall.)
+
+
+// 1.16
+//BMI = mass / height ** 2 = mass / (height * height)//(mass in kg and height in meter)
+
+/*
+let markMass = 78;
+let markHeight = 1.69;
+let johnMass = 92;
+let johnHeight = 1.95;
 */
+
 
 /*
 const massJim = 55;
@@ -106,16 +116,69 @@ is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark
 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
 BMI (28.3) is higher than John's (23.9)!"
 Hint: Use an if/else statement 😉
-*/
 
+
+// challenge #2
 if (markBMI > johnBMI) {
     console.log(`$(markBMI) is higher than John's`);
 } else {
     console.log(`John is higher than Mark's`)
 };
+*/
 
-// 1.18
+//1.25 challenge #3
+
+/*
+
+const scoreDolphins = (96 + 108 + 89) / 3 + ` (Dolphins)`;
+const scoreKoalas = (88 + 91 + 110) / 3 + ` (Koalas )`;
+
+console.log(scoreDolphins, scoreKoalas);
+
+
+if (scoreDolphins > scoreKoalas) {
+    console.log('Dolphins win');
+} else if ('scoreKoalas > scoreDolphins') {
+    console.log('Koalas win');
+} else if ('scoreDolphins === scoreKoalas') {
+    //     console.log("It's a draw!");
+}
 
 
 
+
+
+
+// Bonus1
+const scoreDolphins1 = (97 + 112 + 101) / 3 + ` (Dolphins1)`;
+const scoreKoalas1 = (109 + 95 + 123) / 3 + ` (Koalas 1)`;
+console.log(scoreDolphins1, scoreKoalas1);
+
+
+if (scoreDolphins1 > scoreKoalas1 && scoreDolphins1 >= 100) {
+    console.log('Dolphins win');
+} else if (scoreKoalas1 > scoreDolphins1 && scoreKoalas1 >= 100) {
+    console.log('Koalas win');
+} else if (scoreDolphins1 === scoreKoalas1) {
+    console.log("It's a draw!");
+}
+
+
+
+// Bonus2
+const scoreDolphins2 = (97 + 112 + 50) / 3;
+const scoreKoalas2 = (109 + 95 + 50) / 3;
+console.log(scoreDolphins2, scoreKoalas2);
+
+if (scoreDolphins2 > scoreKoalas2 && scoreDolphins2 >= 100) {
+    console.log('Dolphins win');
+} else if (scoreKoalas2 > scoreDolphins2 && scoreKoalas2 >= 100) {
+    console.log('Koalas win');
+} else if (scoreDolphins2 === scoreKoalas2 && scoreDolphins2 >= 100) {
+    console.log("It's a draw!");
+} else {
+    console.log('No one wins!');
+}
+
+*/
 
