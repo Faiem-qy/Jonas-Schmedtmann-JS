a5b1550ff3adc0955733a88e1a72e367f0d9944d
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-08-02 11:04:55 -0400

    Update script.js

--- Fundamentals-Part1-1.11/script.js
+++ Fundamentals-Part1-1.11/script.js
@@ -112,6 +112,142 @@ const averageAge = (ageJonas + ageSarah) / 2
 console.log(ageJonas, ageSarah, averageAge);
 */
 
+/*
+const firstName = 'Jonas';
+const job = 'teacher'
+const birthYear = 1991;
+const year = 2037;
+
+
+const jonas = "i'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + (job) + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}! `;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log(`String with\n\
+multiple\n\
+lines`);
+
+console.log(`String
+multiple
+lines`);
+/*
+
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda"
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let jonas_matilda = "JM"
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun)
+
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+
+year = 1991;
+console.log(typeof year)
+
+console.log(typeof null);
+
+*/
+
+
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthyear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schemtmann';
+console.log(lastName);
+*/
+
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah,);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 = 8
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x *4 = 100
+x++;// x = x + 1
+x--;//
+x--;//
+console.log(x);
+
+
+console.log(ageJonas > ageSarah);// >,<,>=,<=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+
+let x, y;
+x = y = 25 - 10 - 5;// x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge);
+*/
+
 
 
 // 1.16
@@ -308,7 +444,7 @@ const favourite = Number(prompt(" What's your favourite number?"));
 console.log(favourite);
 console.log(typeof favourite);
 
-if (favourite === 23) {//22 === 23 -> flase
+if (favourite === 23) {//22 === 23 -> false
     console.log('Cool! 23 is an amazing number!');
 } else if (favourite === 7) {
     console.log('7 is also a cool number')
@@ -359,3 +495,4 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 */
 
 //1.25 
+
