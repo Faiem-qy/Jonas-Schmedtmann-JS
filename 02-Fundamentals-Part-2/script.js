4cb3d4fcf75958856b6a36a0c7fff277c319f96a
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-08-04 22:20:20 -0400

    Add files via upload

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+// const interface = 'Audio';
+// const private = 534;
+
+function logger() {
+    console.log('My name id Jonaas');
+}
+
+// calling/running/invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(5, 0);// these parameters are called the arguement
+
