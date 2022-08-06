61d651a0c0033423e050800d79d4ec97ff4131b6
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-08-05 23:00:51 -0400

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -77,19 +77,39 @@
 
 // 3.6 Functions calling other Functions
 
-function cutFruitPieces(fruit) {
-    return fruit * 4;
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+//     const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} piece of orange.`;
+//     return juice;
+// }
+// console.log(fruitProcessor(2, 3));
+
+// 3.7 Reviewing Functions
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
 }
 
-function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
 
-    const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} piece of orange.`;
-    return juice;
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
 }
-console.log(fruitProcessor(2, 3));
 
-// 3.7
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
 
 
