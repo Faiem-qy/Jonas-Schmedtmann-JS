b356beabb944bdf1b4d074411969b51cfc299ec7
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-08-06 23:49:48 -0400

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -11,7 +11,7 @@
 
 // // 3.3 Functions
 // function logger() {
-//     console.log('My name id Jonas');
+//     console.log('My name is Jonas');
 // }
 
 // // calling/running/invoking the function
@@ -92,24 +92,150 @@
 
 // 3.7 Reviewing Functions
 
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const yearsUntilRetirement = function (birthYear, firstName) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+
+//     if (retirement > 0) {
+//         console.log(`${firstName} retires in ${retirement} years`);
+//         return retirement;
+//     } else {
+//         console.log(`${firstName} has already retired`);
+//         return -1;
+//     }
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1950, 'Mike'));
+
+
+
+// function dolphinsScore1(score1, score2, score3) {
+//     console.log(score1, score2, score3);
+//     const score = `The dolphins average score is ${(score1 + score2 + score3) / 3} `;
+//     return score;
+// };
+
+// const firstScore = dolphinsScore1(44, 23, 71);
+// console.log(firstScore);
+// // console.log(dolphinsScore1(44, 23, 71));
+
+
+// const secondScore = dolphinsScore1(85, 54, 41);
+// console.log(secondScore);
+
+
+// const calcAverage = (a, b, c) => ((a + b + c) / 3);
+// console.log(calcAverage(3, 4, 5));
+
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
+
+// const scoreDolphins2 = calcAverage(85, 54, 41);
+// const scoreKoalas2 = calcAverage(23, 34, 27);
+// console.log(scoreDolphins2, scoreKoalas2);
+
+
+// const checkWinner = function (avgDolphins, avgKoalas) {
+//     if (avgDolphins >= 2 * avgKoalas) {
+//         console.log(`Dolphins win the trophy⚡(${avgDolphins} vs ${avgKoalas})`);
+//     } else if (avgKoalas >= 2 * avgDolphins) {
+//         console.log(`Koalas win the trophy⚡(${avgKoalas} vs {avgDolphins})`);
+//     } else {
+//         console.log(' No team wins...');
+//     }
+// }
+
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// checkWinner(5200, 1001);
+
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
+
+/*
+// 3.9 Arrays
+
+const friend1 = 'Michael';
+const friend2 = ' Steven';
+const friend3 = ' Peter';
+
+const friends = ['Micheal', 'Steven', 'Peter'];
+console.log(friends)
+
+
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);// counting elements in the array
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';//changing an element #2 even though it is declared by const
+console.log(friends);
+// friends = ['Bob, 'Alice];// this would be illegal
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+
 const calcAge = function (birthYear) {
     return 2037 - birthYear;
 }
+const yearss = [1990, 1967, 2002, 2010, 2018];
 
-const yearsUntilRetirement = function (birthYear, firstName) {
-    const age = calcAge(birthYear);
-    const retirement = 65 - age;
+// calcAge(yearss);// cannot work because because it is trying to subtract all the years from 2037
+// // cannot do operations with arrays
 
-    if (retirement > 0) {
-        console.log(`${firstName} retires in ${retirement} years`);
-        return retirement;
-    } else {
-        console.log(`${firstName} has already retired`);
-        return -1;
-    }
+const age1 = calcAge(yearss[0]);
+const age2 = calcAge(yearss[1]);
+const age3 = calcAge(yearss[yearss.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(yearss[0]), calcAge(yearss[0]), calcAge(yearss[years.length - 1])];
+console.log(ages);
+*/
+
+// 4. Basic array operations
+const friends = ['Michael', 'Steven', 'Peter'];
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// remove elements
+friends.pop();// last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift();// add elements
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));// because Bob is not in the list then it returns a value of -1
+
+friends.push(23);
+console.log(friends.includes('Steven'));//ES6 method
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+// use the includes method to add conditionals
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven');
 }
-
-console.log(yearsUntilRetirement(1991, 'Jonas'));
-console.log(yearsUntilRetirement(1950, 'Mike'));
 
 
