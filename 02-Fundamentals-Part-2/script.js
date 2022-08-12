b89aa08de71e7795b4a2f7d94ad11e7833fdeb71
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-08-11 20:24:27 -0400

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -313,3 +313,46 @@ console.log(bills, tips, totals);
 // // "Jonas has 3 friends, and his best friend is called Michael"
 // console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
 
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthyear: 1991,
+    job: 'teacher',//string
+    friends: ['Michael', 'Peter', 'Steven'],//array
+    hasDriversLicense: true,//boolean
+
+    // calcAge: function (birthyear) {
+    // function as a parameter
+    // return 2037 - birthyear;
+    //   }
+
+    // calcAge: function () {
+    //     // console.log(this);
+    //     return 2037 - this.birthyear;// using the this keyword connects the object to the method
+
+    calcAge: function () {
+        this.age = 2037 - this.birthyear;
+        return this.age;
+    },
+
+    getSmmary: function () {// challenge
+        return `${this.firstName} is a ${this.calcAge()} - year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    }
+}
+// by using objects we can give each element a name
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// console.log(jonas['calcAge'](1991));
+
+// challenge 
+// "Jonas is a 46-year old teacher, and he has a driver's license.
+
+
+console.log(jonas.getSmmary());// for challenge to be executed
+
