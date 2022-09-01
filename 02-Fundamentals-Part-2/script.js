9d1e2a025ad0559f81d91aedf983019a4e0f3d5a
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-08-31 22:08:27 -0400

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -667,3 +667,26 @@ while (card !== 52) {
     if (card === 42) console.log('you got #42');
 }
 */
+
+// 5.0 
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 :
+        bill * 0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+
+}
