f758cbcf7237681d22023e4d40cd4971e6cbee86
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-06 22:27:10 -0400

    Update assignments.js

---
+++ 03-Developer-Skills/starter/assignments.js
@@ -0,0 +1,17 @@
+const temperatures = [7, 14, 55, 21, 12, 56];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp > min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
