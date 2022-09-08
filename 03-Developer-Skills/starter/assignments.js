3272ac00874d75287e41992d59c4dc7244645a89
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-07 23:09:51 -0400

    Update assignments.js

--- 03-Developer-Skills/starter/assignments.js
+++ 03-Developer-Skills/starter/assignments.js
@@ -15,3 +15,23 @@ const calcTempAmplitude = function (temps) {
 
 const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
+
+//1) Understand the problem
+//-Array transformed to string and separated by ...
+// - What is the X days? Answer: index + 1
+
+// 2) Breaking up into sub partitions
+//- Transform array into a string
+//- Transform each element to string with ºC
+//Strings needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log(`...${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {}
+};
