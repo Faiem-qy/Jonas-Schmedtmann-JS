08d9f2481f7b07ba71ebf8fec25357b8ceace7d7
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-14 22:52:57 -0400

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 
 console.log(document.querySelector('.message').textContent);
 
@@ -11,3 +12,18 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+// const x = function () {
+//   console.log(44);
+// };
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'NO Number!';
+  }
+});
