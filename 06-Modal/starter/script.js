b7c956a70133c22028ac5e44c70612fa6a8902b4
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-20 20:18:50 -0400

    Update script.js

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const modal = document.querySelector('modal');
-const overlay = document.querySelector('overlay');
-const btnCLoseModal = document.querySelector('.closeModal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
@@ -10,5 +10,18 @@ for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', function () {
     console.log('Button clicked');
     modal.classList.remove('hidden'); //remove/add classes?
-    overlay.classList.remove('hidden');?
+    overlay.classList.remove('hidden');
   });
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
