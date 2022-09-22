ca44ace71167f41b63f12376cbeff556b57cf62e
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-21 20:45:13 -0400

    Update script.js

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -8,7 +8,7 @@ console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
+    // console.log('Button clicked');
     modal.classList.remove('hidden'); //remove/add classes?
     overlay.classList.remove('hidden');
   });
@@ -25,3 +25,13 @@ overlay.addEventListener('click', closeModal);
 //   modal.classList.add('hidden');
 //   overlay.classList.add('hidden');
 // });
+
+// 8.1
+document.addEventListener('keydown', function (e) {
+  // console.log('A key was pressed');
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
