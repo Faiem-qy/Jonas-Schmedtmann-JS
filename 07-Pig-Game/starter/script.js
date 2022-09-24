9fa2458a8ee6233d978f3ce7c1891f41bcbf50e9
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-23 22:15:03 -0400

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,23 +12,37 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //starting conditions
-score0El.textContent = 0;
-score1El.textContext = 0;
-diceEl.classList.add('hidden');
+btnNew.addEventListener('click', function () {
 
-const scores = [0.0];
-let currentScore = 0;
-let activePlayer = 0;
+const init = function () {
+  const scores = [0, 0];
+  let currentScore = 0;
+  let activePlayer = 0;
+  let playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContext = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+  };
+init();
+
 
 //rolling dice functionality
-btnRoll.addEventListener('click', function () {
-  // 1.Generating a randome dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
-  //2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+// 1.Generating a randome dice roll
+const dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+//2. Display dice
+diceEl.classList.remove('hidden');
+diceEl.src = `dice-${dice}.png`;
 
+btnRoll.addEventListener('click', function () {
   //3. Check for rolled 1: if true, switch to next player
   if (dice !== 1) {
     //add dice to the current score
