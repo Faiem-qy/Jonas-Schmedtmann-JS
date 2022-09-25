35b2b6352e3a089e6dfd699375de0dbd30539ae4
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-24 23:33:52 -0400

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0'); //El is for element
 const score1El = document.getElementById('score--1'); //get element by ID
 const current0El = document.getElementById('current--0');
@@ -11,33 +13,36 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-//starting conditions
-btnNew.addEventListener('click', function () {
-
 const init = function () {
   const scores = [0, 0];
   let currentScore = 0;
   let activePlayer = 0;
   let playing = true;
 
-    score0El.textContent = 0;
-    score1El.textContext = 0;
-    current0El.textContent = 0;
-    current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContext = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
 
-    diceEl.classList.add('hidden');
-    player0El.classList.remove('player--winner');
-    player1El.classList.remove('player--winner');
-    player0El.classList.add('player--active');
-    player1El.classList.remove('player--active');
-  };
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
 init();
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //rolling dice functionality
 // 1.Generating a randome dice roll
 const dice = Math.trunc(Math.random() * 6) + 1;
-console.log(dice);
 //2. Display dice
 diceEl.classList.remove('hidden');
 diceEl.src = `dice-${dice}.png`;
@@ -46,15 +51,28 @@ btnRoll.addEventListener('click', function () {
   //3. Check for rolled 1: if true, switch to next player
   if (dice !== 1) {
     //add dice to the current score
-    currentScore = currentScore += dice;
+    currentScore += dice;
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
 
     // current0El.textContent = currentScore; // change later from this to the above
   } else {
     //switch to the next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. add current score to active player's score
+  scores[activePlayer] += currentScore;
+
+  // scores[1] = scores[1] + currentScore
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  //2. Check if player' score is >= 100
+  // finish game
+
+  // switch to the next player
+  switchPlayer();
 });
