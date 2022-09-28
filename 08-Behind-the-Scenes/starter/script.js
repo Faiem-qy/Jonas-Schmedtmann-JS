25d60782e48a867c84363719e4905282fce2447b
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-27 21:54:41 -0400

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const name = 'Jonas';
+
+const first = () {
+    let a = 1;
+    const b = second();
+a = a + b;
+return a;
+}
+function secons () {
+    var c = 2;
+    return c;
+}
