efc88c97f85ec3d6cd2779c9c4bc5c5cd299fd79
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-28 22:38:11 -0400

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const name = 'Jonas';
 
 const first = () {
@@ -12,3 +13,18 @@ function secons () {
     var c = 2;
     return c;
 }
+*/
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    const output = ` You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
