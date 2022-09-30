fd89ddd3c54f60f3c04fda672a4d549d6ff79708
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-30 10:59:01 -0400

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -14,6 +14,7 @@ function secons () {
     return c;
 }
 */
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   function printAge() {
@@ -50,3 +51,46 @@ calcAge(1991);
 
 // console.log(age); both of these cannot be called out of the scope
 // printAge();only from within
+*/
+
+//Variables
+console.log(me);
+// console.log(job);
+// console.log(year)
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+//Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+//Example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
