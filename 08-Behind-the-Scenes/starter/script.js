1de8633027d35b08c388738f5b8379c2ccb2ad6a
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-09-29 22:04:29 -0400

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -17,7 +17,26 @@ function secons () {
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   function printAge() {
-    const output = ` You are ${age}, born in ${birthYear}`;
+    const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      //Creating New variable with same name as outwe scope's variable
+      const firstName = 'Steven';
+
+      //Reassigning outer scope's variable
+      const output = 'NEW OUTPUT!';
+      const str = ` Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3)); if you remove strict mode you will get the andwer of 5
     console.log(output);
   }
 
@@ -28,3 +47,6 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+
+// console.log(age); both of these cannot be called out of the scope
+// printAge();only from within
