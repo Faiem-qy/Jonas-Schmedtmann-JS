aa0d336616984d64e2b3038a7c59a5f640aac4fe
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-10-06 22:37:17 -0400

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,10 +12,6 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
   openingHours: {
     thu: {
       open: 12,
@@ -29,9 +25,13 @@ const restaurant = {
       open: 0, // Open 24 hours
       close: 24,
     },
+    order: function (starterIndex, mainIndex) {
+      return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
   },
 };
 
+/*
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -62,7 +62,40 @@ console.log(main, secondary);
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
-const nested = [2, (4)[(5, 6)]];
+//nested destructuring
+const nested = [2, (4)[(5, 6)]]; //?
 // const [i, , j] = nested;
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
+*/
+
+// default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+
+// 10.4
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//Mutating variables
+
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj); //ad parenthesis to make it work
+console.log(a, b);
+
+// nested objects
+const {
+  fri: { open = o, close = c },
+} = openingHours;//?
+console.log(o, c);
