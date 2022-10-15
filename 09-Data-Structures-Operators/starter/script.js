fca6e2c05c546a3f359af2fece8427c911b1448e
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-10-14 22:50:47 -0400

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -299,12 +299,12 @@ const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firezne, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  startMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  // order: function (starterIndex, mainIndex) {
-  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  // },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
 
 const arr = [2, 3, 4];
@@ -336,5 +336,7 @@ const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
 const nested = [2, 4, [5, 6]];
-const [i, , j] = nested;
-console.log(i, j);
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
