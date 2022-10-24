505b5bc22e7e2030f0d9b0239b7e686828db086b
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-10-23 22:50:11 -0400

    Update script.js

--- Recap 1 - 5/script.js
+++ Recap 1 - 5/script.js
@@ -146,3 +146,40 @@ console.log(x, y);
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
 */
+
+//1.16 coding exercise
+
+//Test Data 1
+// let markMass = 78;
+// let markHeight = 1.69;
+
+// let johnMass = 92;
+// let johnHeight = 1.95;
+
+// Test Data 2
+// let markMass = 95;
+// let markHeight = 1.88;
+
+// let johnMass = 85;
+// let johnHeight = 1.76;
+
+// Test Data 3
+let markMass = 200;
+let markHeight = 1.88;
+
+let johnMass = 85;
+let johnHeight = 1.76;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+console.log(markBMI, johnBMI);
+
+if (markBMI > johnBMI) {
+  console.log(
+    `Mark's BMI is ${markBMI} and John's BMI is ${johnBMI}. The results indicate that Mark's BMI is higher`
+  );
+} else {
+  console.log(
+    `John's BMI is ${johnBMI} and Mark's BMI is ${markBMI}. The results indicate that John's BMI is higher`
+  );
+}
