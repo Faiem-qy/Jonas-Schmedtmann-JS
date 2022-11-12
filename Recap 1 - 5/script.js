f334da2320d160632d53d1d11cae3a6c68bce307
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-11-11 22:44:49 -0500

    Update script.js

--- Recap 1 - 5/script.js
+++ Recap 1 - 5/script.js
@@ -762,16 +762,39 @@ console.log(
 
 //1.36 Functions calling other functions
 
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
 
-  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
-  return juice;
-}
+//   const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+//   return juice;
+// }
 
-console.log(fruitProcessor(2, 3));
+// console.log(fruitProcessor(2, 3));
+
+//1.37 Reviewing Functions
+
+const calcAge11 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge11(birthYear);
+  const retirement = 65 - age;
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} is already retired`);
+    return -1;
+  }
+
+  return retirement;
+  // return `${firstName} retires in ${retirement} years`;
+};
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
