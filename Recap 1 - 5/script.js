f69e58c898b9e68badb6543f71a9bd60d4da05bc
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-11-18 23:31:40 -0500

    Update script.js

--- Recap 1 - 5/script.js
+++ Recap 1 - 5/script.js
@@ -998,3 +998,33 @@ console.log(
 // console.log(
 //   `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
 // );
+
+//1.44 Object Methods
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYear: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: true,
+
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+
+  // calcAge: function () {
+  //   console.log(this);
+  //   return 2037 - this.birthYear;
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+};
+
+console.log(jonas.calcAge()); //using the 'this'keyword
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// console.log(jonas['calcAge'](1991));
