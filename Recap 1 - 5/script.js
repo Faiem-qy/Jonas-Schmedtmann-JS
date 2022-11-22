bb60c40ba65eff209b310f1e19bfcde6879c717a
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-11-21 22:22:20 -0500

    Update script.js

--- Recap 1 - 5/script.js
+++ Recap 1 - 5/script.js
@@ -1044,37 +1044,80 @@ console.log(
 
 // BMI = mass/height **2 = mass/ (height *height)
 
-const mark = {
-  fullName: 'Mark Miller',
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    this.bmi = this.mass / this.height ** 2;
-    return this.bmi;
-  },
-};
+// const mark = {
+//   fullName: 'Mark Miller',
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+//   },
+// };
 
-const john = {
-  fullName: 'John Smith',
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.bmi = this.mass / this.height ** 2;
-    return this.bmi;
-  },
-};
+// const john = {
+//   fullName: 'John Smith',
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+//   },
+// };
 
-mark.calcBMI();
-john.calcBMI();
+// mark.calcBMI();
+// john.calcBMI();
 
-console.log(mark.bmi, john.bmi);
+// console.log(mark.bmi, john.bmi);
 
-if (mark.bmi > john.bmi) {
-  console.log(
-    ` ${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s bmi (${john.bmi})`
-  );
-} else if (john.bmi > mark.bmi) {
-  console.log(
-    `  ${john.fullName}'s BMI (${john.bmi}) is  higher than ${mark.fullName}'s BMI (${mark.bmi})`
-  );
+// if (mark.bmi > john.bmi) {
+//   console.log(
+//     ` ${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s bmi (${john.bmi})`
+//   );
+// } else if (john.bmi > mark.bmi) {
+//   console.log(
+//     `  ${john.fullName}'s BMI (${john.bmi}) is  higher than ${mark.fullName}'s BMI (${mark.bmi})`
+//   );
+// }
+
+// 1.46 Looping
+
+// eg
+
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+// console.log('Lifting weights repetition 7');
+// console.log('Lifting weights repetition 8');
+// console.log('Lifting weights repetition 9');
+// console.log('Lifting weights repetition 10');
+
+// for loop keep running while condition is true
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+
+// 1.47
+
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true,
+  true,
+];
+
+console.log(jonas[0]);
+console.log(jonas[1]);
+console.log(jonas[2]);
+console.log(jonas[3]);
+console.log(jonas[4]);
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i]);
 }
