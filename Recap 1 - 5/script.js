e309798cc7774c522bb3926e1362d246dc05ab8d
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-11-25 23:35:50 -0500

    Update script.js

--- Recap 1 - 5/script.js
+++ Recap 1 - 5/script.js
@@ -1102,52 +1102,52 @@ console.log(
 
 // // 1.47
 
-const jonas = [
-  'Jonas',
-  'Schmedtmann',
-  2037 - 1991,
-  'teacher',
-  ['Michael', 'Peter', 'Steven'],
-  true,
-  true,
-];
+// const jonas = [
+//   'Jonas',
+//   'Schmedtmann',
+//   2037 - 1991,
+//   'teacher',
+//   ['Michael', 'Peter', 'Steven'],
+//   true,
+//   true,
+// ];
 
-const types = [];
-// console.log(jonas[0]);
-// console.log(jonas[1]);
-// console.log(jonas[2]);
-// console.log(jonas[3]);
-// console.log(jonas[4]);
+// const types = [];
+// // console.log(jonas[0]);
+// // console.log(jonas[1]);
+// // console.log(jonas[2]);
+// // console.log(jonas[3]);
+// // console.log(jonas[4]);
 
-for (let i = 0; i < jonas.length; i++) {
-  console.log(jonas[i], typeof jonas[i]);
+// for (let i = 0; i < jonas.length; i++) {
+//   console.log(jonas[i], typeof jonas[i]);
 
-  // Filling types of array
-  // types[i] = typeof jonas[i];
+//   // Filling types of array
+//   // types[i] = typeof jonas[i];
 
-  types.push(typeof jonas[i]);
-}
+//   types.push(typeof jonas[i]);
+// }
 
-console.log(types);
+// console.log(types);
 
-const years = [1991, 2007, 1969, 2020];
-const ages = [];
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
 
-for (let i = 0; i < years.length; i++) {
-  ages.push(2037 - years[i]);
-}
-console.log(ages);
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i]);
+// }
+// console.log(ages);
 
-// continue and break
-console.log('------------Only Strings------------');
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] !== 'string') continue;
-  console.log(jonas[i], typeof [i]);
-}
+// // continue and break
+// console.log('------------Only Strings------------');
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] !== 'string') continue;
+//   console.log(jonas[i], typeof [i]);
+// }
 
-// continue and break
-console.log('------------Breal With Number------------');
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] !== 'number') break;
-  console.log(jonas[i], typeof [i]);
-}
+// // continue and break
+// console.log('------------Breal With Number------------');
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] !== 'number') break;
+//   console.log(jonas[i], typeof [i]);
+// }
