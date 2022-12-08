4d3b072719859e1d4d4d5740e804b75ebf35ca9f
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-12-07 23:34:33 -0500

    Update script.js

--- Recap 1 - 5/script.js
+++ Recap 1 - 5/script.js
@@ -1348,17 +1348,17 @@ console.log(amplitudeBug);
 // - add... between elements and start and end of string
 //- log sring to console
 
-const data1 = [17, 21, 23];
-const data2 = [12, 5, -5, 0, 4];
+// const data1 = [17, 21, 23];
+// const data2 = [12, 5, -5, 0, 4];
 
-console.log(`...${data1[0]} oC ...${data1[1]} oC ...${data1[2]} oC ...`);
+// console.log(`...${data1[0]} oC ...${data1[1]} oC ...${data1[2]} oC ...`);
 
-const printForecast = function (arr) {
-  let str = '';
-  for (let i = 0; i < arr.length; i++) {
-    str = str + `${arr[i]} oC in ${i + 1} days... `;
-  }
-  console.log('...' + str);
-};
+// const printForecast = function (arr) {
+//   let str = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     str = str + `${arr[i]} oC in ${i + 1} days... `;
+//   }
+//   console.log('...' + str);
+// };
 
-printForecast(data1);
+// printForecast(data1);
