7b9bcccd3dccaf1262141d9b195a3796393f69ee
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-12-11 16:41:18 -0500

    Update script.js

--- Recap 1 - 5/script.js
+++ Recap 1 - 5/script.js
@@ -1371,3 +1371,72 @@ const logCitySkyline = function(){
 };
 
 console.log(logCitySkyline());
+
+
+const hobbies = ['swim','play','drive'];
+
+console.log(hobbies);
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+
+const listItem = famousSayings[0];
+console.log(listItem);
+
+console.log(famousSayings[2]);
+
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+
+groceryList[1] = 'avocados';
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+console.log(utensils);
+
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+chores.push('cook food', 'eat food')
+console.log(chores);
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+
+// groceryList.shift();
+// console.log(groceryList);
+
+// groceryList.unshift('popcorn');
+// console.log(groceryList);
+
+// console.log(groceryList.slice(1,4));
+// console.log(groceryList);
+const pastaIndex = groceryList.indexOf('pasta')
+console.log(pastaIndex);
+
+
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+console.log(concept);
+
+const removeElement = function (newArr){
+newArr.pop()
+};
+removeElement(concept);
+console.log(removeElement);
+console.log(concept);
+
+
+const numberClusters = [[1,2],[3,4],[5,6]];
+const target=numberClusters[2][1];
