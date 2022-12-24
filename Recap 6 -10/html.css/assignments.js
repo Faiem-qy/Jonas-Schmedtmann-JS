2a45209a1b972407555cb437677c572215747cdc
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-12-23 22:00:20 -0500

    Update assignments.js

--- Recap 6 -10/html.css/assignments.js
+++ Recap 6 -10/html.css/assignments.js
@@ -17,17 +17,37 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+
+    // when player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = ' Correct Number!';
+    document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // when guess is too high
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = ' Too High !!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!!';
+      document.querySelector('.score').textContent = 0;
+    }
+    // when guess is too low
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = ' Too Low!!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' Too Low!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
