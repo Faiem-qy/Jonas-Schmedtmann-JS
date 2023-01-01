0608913b8e19b9239bc70ee0447eb2934c657bf9
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2022-12-31 22:53:23 -0500

    Update script.js

--- Recap 6 -10/html.css/07-Pig-Game/starter/script.js
+++ Recap 6 -10/html.css/07-Pig-Game/starter/script.js
@@ -21,6 +21,7 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -32,45 +33,51 @@ const switchPlayer = function () {
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  //1. Generating a random roll
-  const dice = Math.trunc(Math.random() * 6) + 1; //makes  0 - 5 add 1 to make 6
+  if (playing) {
+    //1. Generating a random roll
+    const dice = Math.trunc(Math.random() * 6) + 1; //makes  0 - 5 add 1 to make 6
 
-  //2. Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    //2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  //3.Check for rolled 1: if true, switch to next player
-  if (dice !== 1) {
-    //Add dice to current score
-    currentScore += dice; // shortcut for currentScore = dice
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    current0El.textContent = currentScore; // change later
-  } else {
-    // Switch to the next player
-    switchPlayer();
+    //3.Check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      //Add dice to current score
+      currentScore += dice; // shortcut for currentScore = dice
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      current0El.textContent = currentScore; // change later
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
   scores[activePlayer] += currentScore;
+  if (playing) {
+    // 1. add current score to active player's score
+    // scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-  // 1. add current score to active player's score
-  // scores[1] = scores[1] + currentScore
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
-
-  //2. Check if player's score is >= 100
-  if (scores[activePlayer] >= 20) {
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('Player--winner');
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('Player--active');
-  } else {
-    // Switch to the next player
-    switchPlayer();
+    //2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+      //Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+    //Finish the game
   }
-  //Finish the game
 });
