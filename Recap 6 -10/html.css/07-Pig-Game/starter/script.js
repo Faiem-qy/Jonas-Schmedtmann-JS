b064422f9945496d9ea43820e54abbc8971f86df
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2023-01-01 22:13:10 -0500

    Update script.js

--- Recap 6 -10/html.css/07-Pig-Game/starter/script.js
+++ Recap 6 -10/html.css/07-Pig-Game/starter/script.js
@@ -14,14 +14,31 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// score0El.textContent = 0; duplicated in the init function
+// score1El.textContent = 0;  duplicated in the init function
+// diceEl.classList.add('hidden'); duplicated in the init function
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -47,7 +64,6 @@ btnRoll.addEventListener('click', function () {
       currentScore += dice; // shortcut for currentScore = dice
       document.getElementById(`current--${activePlayer}`).textContent =
         currentScore;
-      current0El.textContent = currentScore; // change later
     } else {
       // Switch to the next player
       switchPlayer();
@@ -64,7 +80,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -81,3 +97,4 @@ btnHold.addEventListener('click', function () {
     //Finish the game
   }
 });
+btnNew.addEventListener('click', init);
