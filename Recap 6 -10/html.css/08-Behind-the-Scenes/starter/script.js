5beb678566c3951a34dd58383617d552588e97ba
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2023-01-07 21:59:22 -0500

    Update script.js

--- Recap 6 -10/html.css/08-Behind-the-Scenes/starter/script.js
+++ Recap 6 -10/html.css/08-Behind-the-Scenes/starter/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 /*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
@@ -87,10 +87,11 @@ console.log(y === window.x);
 console.log(z === window.x);
 
 */
+/*
 // 8.96 The This keyword
 
 const bob = {
-  name: "Bob",
+  name: 'Bob',
   year: 1989,
   calcAge: function () {
     return 2037 - this.year; // this.year has the same effect of sayig Bob.year
@@ -109,7 +110,7 @@ const calcAge = function (birthYear) {
 calcAge(1991);
 // ---------------------------
 // arrow function uses the lexical this keyword, it uses the this version from the parent scope
-const calcAgeArrow = (birthYear) => {
+const calcAgeArrow = birthYear => {
   console.log(2037 - birthYear);
   console.log(this);
 };
@@ -127,9 +128,102 @@ jonas.calcAge();
 const matilda = {
   year: 2017,
 };
-
 matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
+*/
 
-const f = jonas.calcAge;
-f();
+/*
+// 8.98 Regular function vs arrow functions
+// arrow functions do not get the this keyword
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+
+//   greet: () => console.log(`Hey ${this.firstName}`),
+// };
+// jonas.greet();
+
+// -----------
+
+// having a function inside a method
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+
+    // const self = this; // self is used to preserve the this keyword...self or that. pre es6 solution.
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    //   // console.log(this.year >= 1981 && this.year <= 1996);
+    // };
+
+    /*
+    // Solution 2 the arrow function does not have a this keyword
+
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword - just like the this only available in regular functions
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+
+addArrow(2, 5, 8);
+*/
+
+// 8.99 primitive vs reference types  (recap)
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age);
+// console.log(oldAge);
+
+// scenario with Object
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log(' Friend:', friend);
+console.log('Me', me);
