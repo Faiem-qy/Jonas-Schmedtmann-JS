d5ab2dcb8fd41642dc87a9af2d98a1e743123b9c
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2023-01-08 23:16:26 -0500

    Update script.js

--- Recap 6 -10/html.css/08-Behind-the-Scenes/starter/script.js
+++ Recap 6 -10/html.css/08-Behind-the-Scenes/starter/script.js
@@ -204,6 +204,7 @@ var addArrow = (a, b) => {
 addArrow(2, 5, 8);
 */
 
+/*
 // 8.99 primitive vs reference types  (recap)
 // let age = 30;
 // let oldAge = age;
@@ -225,5 +226,43 @@ const me = {
 };
 const friend = me;
 friend.age = 27;
-console.log(' Friend:', friend);
+console.log('Friend:', friend);
 console.log('Me', me);
+*/
+// 8.100 Primitives vs objects in practice
+
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+const jessica = {
+  firstName: ' Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage: ', marriedJessica);
+
+// marriedJessica = {} will not work because it is declared using const
+
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+// Object.assign only works on the first level
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
