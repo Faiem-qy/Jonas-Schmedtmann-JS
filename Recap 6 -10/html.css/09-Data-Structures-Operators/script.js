19377bf28a965e510928465559ada77b19c574f0
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2023-01-12 22:16:21 -0500

    Update script.js

--- Recap 6 -10/html.css/09-Data-Structures-Operators/script.js
+++ Recap 6 -10/html.css/09-Data-Structures-Operators/script.js
@@ -1,16 +1,12 @@
-'use strict';
-
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+"use strict";
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
   openingHours: {
     thu: {
       open: 12,
@@ -34,6 +30,7 @@ const restaurant = {
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
 
+//default values
 const {
   name: restaurantName,
   openingHours: hours,
@@ -44,8 +41,23 @@ console.log(restaurantName, hours, tags);
 const { menu = [], starterMenu: starters = [] } = restaurant;
 console.log(menu, starters);
 
-// 9.103 Array destructuring
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
 /*
+
+// 9.103 Array destructuring
+
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -71,20 +83,17 @@ console.log(main, secondary);
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
 
-// Receive 2 return values from a function
+//Receive 2 return values from a function
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
 
-// Nested Destructuring
+//Nested destructuring
 const nested = [2, 4, [5, 6]];
 // const [i, , j] = nested;
-// console.log(i, j);
-
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
 
 //Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
-
+console.log((p, q, r));
 */
