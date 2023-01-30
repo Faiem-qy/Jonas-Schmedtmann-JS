4f71b317b645dd1c53093a85bc1e0c7f2990a15a
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2023-01-29 21:46:44 -0500

    Update script.js

--- Recap 6 -10/html.css/09-Data-Structures-Operators/script.js
+++ Recap 6 -10/html.css/09-Data-Structures-Operators/script.js
@@ -62,12 +62,43 @@ const capatilizeName = function (name) {
   const namesUpper = [];
 
   for (const n of names) {
-    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
   }
   console.log(namesUpper.join(' '));
 };
 capatilizeName('jessica ann smith davis');
 capatilizeName('jonas schmedtmann');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(30, '+'));
+console.log('Jonas'.padStart(25, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  // padding credit card numbers
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('334859493847755774747'));
+
+//Repeat
+
+const message2 = 'Bad weather ...All Departures Delayed...';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  // example for planes in line at the airport
+  console.log(`There are ${n} planes in line ${'✈'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
 /*
 //9.122 working with strings 2
 const airline = 'Tap Air Portugal';
