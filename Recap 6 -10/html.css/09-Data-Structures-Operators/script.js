1b0a2e83d5f1c4f47f0d984fc737a48929e10c57
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2023-01-31 19:02:08 -0800

    Update script.js

--- Recap 6 -10/html.css/09-Data-Structures-Operators/script.js
+++ Recap 6 -10/html.css/09-Data-Structures-Operators/script.js
@@ -49,6 +49,27 @@ const restaurant = {
   },
 };
 
+//9.125 String methods exercise
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// 🛑 Delayed Departure from FAO to TXL (11h25)
+//               Arrival from BRU to FAO (11h45)
+//   🛑 Delayed Arrival from HEL to FAO (12h05)
+//             Departure from FAO to LIS (12h30)
+const getCode = (str) => str.slice(0, 3).toUpperCase();
+
+console.log(flights.split("+"));
+
+for (const flight of flights.split("+")) {
+  const [type, from, to, time] = flight.split(";");
+  const output = `${type.startsWith("_Delayed") ? "🛑" : " "}${type.replaceAll(
+    "_",
+    " "
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(":", "h")})`.padStart(38);
+  console.log(output);
+}
+
 // 9.124 challenge
 /*
 Write a program that receives a list of variable names written in underscore_case
@@ -77,6 +98,7 @@ you're stuck. Then pause and continue!
 Afterwards, test with your own test data!
 */
 
+/*
 document.body.append(document.createElement("textarea"));
 document.body.append(document.createElement("button"));
 
@@ -94,6 +116,8 @@ document.querySelector("button").addEventListener("click", function () {
     console.log(`${output.padEnd(20)} ${"✅".repeat(i + 1)}`);
   }
 });
+
+*/
 
 // underscore_case
 // first_name
