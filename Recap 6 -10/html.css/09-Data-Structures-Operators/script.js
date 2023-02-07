79302a7a289a937cd2b5cc8ce18d22a663a5728a
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2023-02-06 23:01:55 -0500

    Update script.js

--- Recap 6 -10/html.css/09-Data-Structures-Operators/script.js
+++ Recap 6 -10/html.css/09-Data-Structures-Operators/script.js
@@ -989,17 +989,125 @@ var balance = 0;
 var checkBalance = true;
 var isActive = true;
 
+// // your code goes here
+
+if (checkBalance === true) {
+    // Cases when account is active. Now, the balance could be <, =, or > zero
+    if (isActive === true && balance > 0) {
+        console.log("Your balance is $" + balance.toFixed(2) + ".");
+    }
+    else if (isActive === true && balance === 0){
+        console.log("Your account is empty.");
+    } 
+    else if (isActive === true && balance < 0){
+        console.log("Your balance is negative. Please contact bank.");
+    }
+    // Case when account is NOT active
+    else if (isActive === false) {
+        console.log("Your account is no longer active.");
+    }  
+} 
+else {
+  console.log("Thank you. Have a nice day!");
+}
+
+// Udacity 13
+var flavor = "chocolate";
+var vessel = "cone";
+var toppings = "peanuts";
+
+// Add your code here
+
+if((flavor === "vanilla" || flavor === "chocolate") && (vessel === "cone" || vessel ==="bowl") && (toppings === "sprinkles" || toppings === "peanuts")) {
+    console.log(`I'd like two scoops of ${flavor} ice cream in a ${vessel} with ${toppings}`);
+}
+
+
+//Udacity 14. 
+var shirtWidth = 24;
+var shirtLength = 31;
+var shirtSleeve = 8.88;
+
+/*
+ * To gain confidence, check your code for the following combination of [shirtWidth, shirtLength, shirtSleeve, expectedSize]:
+ * [18, 28, 8.13, 'S']
+ * [19.99, 28.99, 8.379, 'S']
+ * [20, 29, 8.38, 'M']
+ * [22, 30, 8.63, 'L']
+ * [24, 31, 8.88, 'XL']
+ * [26, 33, 9.63, '2XL']
+ * [27.99, 33.99, 10.129, '2XL']
+ * [28, 34, 10.13, '3XL']
+ * [18, 29, 8.47, 'NA']
+*/
+
+// WRITE YOUR CODE HERE
+
+
+    if((shirtWidth >= 18 && shirtWidth < 20) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve >= 8.13 && shirtSleeve <8.38)) { 
+        console.log("S");
+    }else if((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve <8.63)) {
+        console.log("M")
+    }else if((shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >= 30 && shirtLength < 31) && (shirtSleeve >= 8.63 && shirtSleeve <8.88)) {
+        console.log("L")
+    }else if((shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >= 31 && shirtLength < 33) && (shirtSleeve >= 8.88 && shirtSleeve <9.63)) {
+        console.log("XL")
+    }else if((shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >= 33 && shirtLength < 34) && (shirtSleeve >= 9.63 && shirtSleeve <10.13)) {
+        console.log("2XL")
+    }else if((shirtWidth >= 28) && (shirtLength >= 34) && (shirtSleeve >= 10.13)) {
+        console.log("3XL")
+    }else{
+        console.log("NA")
+    }
+
+
+// udacity 18
+var eatsPlants = true;
+var eatsAnimals = true;
+
+/*
+ * Test your code agaist the followig possible input/output combinations of (`eatsPlants`, `eatsAnimals`, expected output):
+ * - (true, true, omnivore)
+ * - (false, true, carnivore)
+ * - (true, false, herbivore)
+ * - (false, false, undefined)
+ */
+
+var category = eatsPlants ?  (eatsAnimals ? "omnivore" : "herbivore" ):(eatsAnimals ? "carnivore" : "undefined");
+console.log(category);
+
+// udacity 21 
+// change the value of `education` to test your code
+var education = 'no high school diploma';
+
+// set the value of this based on a person's education
+var salary = 0;
+
 // your code goes here
 
-if(checkBalance === false) {
-    console.log("Thank you. Have a nice day!");
-}else if(checkBalance === true && isActive === true && balance > 0){
-    console.log("Your balance is $" + balance + ".");
-}else if(checkBalance === true && isActive === false && balance > 0){
-    console.log("Your balance is negative. Please contact the bank");
-}else if(checkBalance === true && isActive === false){
-    console.log("Your account is no longer active.");
-}else if(checkBalance === true && isActive === true && balance === 0){
-    console.log("Your account is empty.");
+switch (education) {
+  case 'no high school diploma':
+    salary = 25636;
+    break;
+  case 'a high school diploma':
+    salary = 35256;
+    break;
+  case "an Associate's degree":
+    salary = 41496;
+    break;
+  case "a Bachelor's degree":
+    salary = 59124;
+    break;
+  case "a Master's degree":
+    salary = 69732;
+    break;
+  case 'a Professional degree':
+    salary = 89960;
+    break;
+  case 'a Doctoral degree':
+    salary = 84396;
+    break;
 }
-//to review
+console.log(
+  'In 2015, a person with ' +education+' earned an average of $'+salary.toLocaleString('en-US')+'/year.'
+);
