977ceaa633f23135e8bf2efe13e4949192cf2eed
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2023-02-11 20:27:01 -0800

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -214,17 +214,56 @@ console.log(addVAT(100));
 console.log(addVAT(23));
 */
 
-//134 the bind method
+/*
+//135 the bind method
 // challenge
 
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1:Python", "2:Rust", "3: C++"],
+
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get amswer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+      )
+    );
+    // Register answer
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults("string");
+  },
+  displayResults(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`Poll results are ${this.answers.join(", ")}`);
+    }
+  },
+};
+
+// poll.registerNewAnswer();
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, "string");
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+*/
+
 /*
-// 135 Immediately Invoked Function Expression
+// 136 Immediately Invoked Function Expression
 const runOnce = function () {
   console.log('This will never run again');
 };
 runOnce();
 
-// 136
+// 137
 // IIFE
 (function () {
   console.log('This will never runa again');
@@ -239,7 +278,7 @@ runOnce();
 }
 console.log(notPrivate);
 */
-// 137 closures
+// 138 closures
 
 const secureBooking = function () {
   let passengerCount = 0;
