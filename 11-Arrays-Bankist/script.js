c3cb43827dd529317464eee9292b816059a83d8f
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2023-02-13 20:04:42 -0800

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -65,6 +65,9 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+/*
+// 142 Simple Array Methods
+
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
@@ -108,3 +111,50 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join(' - '));
+
+*/
+
+/*
+//143
+// the new at mmethod
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting the last arry element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+// the at method also works on strings;
+
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
+
+*/
+//144  Looping arrays forEach Method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//  for (const movement of movements)
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('------forEach---------');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Mov ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Mov ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// 0: function(200)
+// 1: function(400)
+// 2: function(450)
+// ...
