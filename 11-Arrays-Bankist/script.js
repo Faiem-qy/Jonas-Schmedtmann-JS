0372494defbf02961e088038eedc93a7346845fe
Faiem Quan Young <110076359+Faiem-qy@users.noreply.github.com>
2023-03-20 22:36:10 -0400

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -413,3 +413,13 @@ function doubleInteger(i) {
   // i will be an integer. Double it and return it.
   return i*2;
 }
+-----
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+function squareSum(numbers){
+let sum = 0;
+  for(let i = 0; i < numbers.length;i++){
+    sum += Math.pow(numbers[i],2)
+  }
+  return sum
+}
